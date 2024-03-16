0a73510bd1136ec5f7b781a1f8304133d788b7a7
Varun Kumar <heysadboy@gmail.com>
2024-03-16 01:51:09 +0100

    Update grid update logic
    Handle card update on click in grid itself.

--- client/src/components/cardGrid/index.ts
+++ client/src/components/cardGrid/index.ts
@@ -1,7 +1,26 @@
 import { Card, generateCard } from "../card";
 import "./style.css";
 
-export const renderCards = (cards: Card[], correctGuessedList: number[]): void => {
+const correctGuessedList: string[] = [];
+let currentGuess = "";
+let cards: Card[] = [];
+
+const handleCardClick = (card: Card) => {
+	const guessedNumber = Number(card.key.substring(0, card.key.indexOf("_")));
+	const currentGuessNumber = Number(currentGuess.substring(0, currentGuess.indexOf("_")));
+	if (currentGuess === "") {
+		currentGuess = card.key;
+	} else if (currentGuessNumber === guessedNumber && currentGuess !== card.key) {
+		correctGuessedList.push(currentGuess);
+		correctGuessedList.push(card.key);
+		currentGuess = "";
+	} else {
+		currentGuess = "";
+	}
+	updateCards();
+};
+
+const updateCards = () => {
 	const appElement = document.getElementById("app");
 	if (appElement === null) {
 		return;
@@ -9,10 +28,19 @@ export const renderCards = (cards: Card[], correctGuessedList: number[]): void =
 	appElement.innerHTML = "";
 	const gridElement = document.createElement("div");
 	gridElement.className = "grid-container";
-
 	cards.forEach((card) => {
-		const cardKey = Number(card.key.substring(0, card.key.indexOf("_")));
-		gridElement.appendChild(generateCard(card, correctGuessedList.includes(cardKey)));
+		gridElement.appendChild(generateCard(card, correctGuessedList.includes(card.key)));
 	});
 	appElement.appendChild(gridElement);
+	cards.forEach((card) => {
+		const cardById = document.getElementById(`card-container-${card.key}`);
+		if (cardById) {
+			cardById.addEventListener("click", () => handleCardClick(card));
+		}
+	});
+};
+
+export const renderCards = (cardList: Card[]): void => {
+	cards = cardList;
+	updateCards();
 };
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,27 +4,6 @@ import "./style.css";
 import { generateCardList } from "./utility";
 
 const cardNumbersLength = 18;
-const correctGuessedList: number[] = [];
-let currentGuess = 0;
 const cardList: Card[] = generateCardList(cardNumbersLength);
 
-const handleCardClick = (card: Card) => {
-	const guessedNumber = Number(card.key.substring(0, card.key.indexOf("_")));
-	if (currentGuess === 0) {
-		currentGuess = guessedNumber;
-	} else if (currentGuess === guessedNumber) {
-		correctGuessedList.push(guessedNumber);
-		renderCards(cardList, correctGuessedList);
-	} else {
-		currentGuess = 0;
-	}
-};
-
-renderCards(cardList, correctGuessedList);
-
-cardList.forEach((card) => {
-	const cardById = document.getElementById(`card-container-${card.key}`);
-	if (cardById) {
-		cardById.addEventListener("click", () => handleCardClick(card));
-	}
-});
+renderCards(cardList);
